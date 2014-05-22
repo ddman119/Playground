6cad95aa49919dcc5ef6acaecda4237667ec15e7
Sam Symons <sam@samsymons.com>
2014-05-22 10:44:24 -0700

    More chapter 5 sample code.

--- Haskell/Learn You a Haskell/05/filter.hs
+++ Haskell/Learn You a Haskell/05/filter.hs
@@ -1,5 +1,5 @@
-filter :: (a -> Bool) -> [a] -> [a]
-filter _ [] = []
-filter p (x:xs)
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
   | p x = x : filter p xs
   | otherwise = filter p xs
