5d5358e834fd3ffffb74153a94324ac2f6349c8b
Sam Symons <sam@samsymons.com>
2014-06-10 07:40:51 -0700

    Add more answers.

---
+++ Haskell/99 Problems/5.hs
@@ -0,0 +1,5 @@
+-- Original:
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
---
+++ Haskell/99 Problems/6.hs
@@ -0,0 +1,4 @@
+-- Original:
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = (reverse xs) == xs
