17d7badad34f9228c09914681d323767598327ea
Sam Symons <sam@samsymons.com>
2014-06-18 08:13:34 -0700

    Add more samples from chapter 3.

---
+++ Haskell/Real World Haskell/03/AlgebraicVector.hs
@@ -0,0 +1,7 @@
+-- x y coordinate pairs.
+data Cartesian2D = Cartesian2D Double Double
+                   deriving (Eq, Show)
+
+-- Angle and distance.
+data Polar2D = Polar2D Double Double
+               deriving (Eq, Show)
--- Haskell/Real World Haskell/03/BookStore.hs
+++ Haskell/Real World Haskell/03/BookStore.hs
@@ -30,3 +30,14 @@ data BookReview = BookReview BookInfo CustomerID String
 data BetterReview = BetterReview BookInfo CustomerID ReviewBody
 
 type BookRecord = (BookInfo, BookReview)
+
+-- Display billing information:
+
+type CardHolder = String
+type CardNumber = String
+type Address = [String]
+
+data BillingInfo = CreditCard CardNumber CardHolder Address
+                 | CashOnDelivery
+                 | Invoice CustomerID
+                   deriving (Show)
