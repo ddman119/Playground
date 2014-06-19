118823776a5dc9643cd4d1fab2bf3aaea6b30a72
Sam Symons <sam@samsymons.com>
2014-06-18 21:03:46 -0700

    Messing around with geometry functions.

--- Haskell/Playground/trapezoidArea.hs
+++ Haskell/Playground/trapezoidArea.hs
@@ -2,6 +2,12 @@ type TopBase = Int
 type BottomBase = Int
 type Height = Int
 
-data Trapezoid = Trapezoid TopBase BottomBase Height
+data Trapezoid = Trapezoid {
+    topBase :: TopBase
+  , bottomBase :: BottomBase
+  , height :: Height
+  } deriving (Show)
 
-trapezoidArea :: Trapezoid -> Double
+trapezoidArea :: Trapezoid -> Int
+trapezoidArea (Trapezoid top bottom height) = height * ((top + bottom) / 2)
+trapezoidArea _ = 0
