0a73071fb73fac98d8d0d10b1c28c7525061ed42
Sam Symons <sam@samsymons.com>
2014-06-21 09:04:16 -0700

    Add conversions to and from the List type.

--- Haskell/Real World Haskell/03/ListADT.hs
+++ Haskell/Real World Haskell/03/ListADT.hs
@@ -2,6 +2,10 @@ data List a = Cons a (List a)
             | Nil
               deriving (Show)
 
-fromList :: [a] -> List a
-fromList (x:xs) = Cons x (fromList xs)
-fromList [] = Nil
+fromArray :: [a] -> List a
+fromArray (x:xs) = Cons x (fromArray xs)
+fromArray [] = Nil
+
+fromList :: List a -> [a]
+fromList (Cons x xs) = x:(fromList xs)
+fromList Nil = []
