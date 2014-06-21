9bbe48287f771ccbf82d4d7ca75b06805c0bb6d8
Sam Symons <sam@samsymons.com>
2014-06-21 12:58:06 -0700

    Fill out half of the exercises from chapter 3.

---
+++ Haskell/Real World Haskell/03/Exercises.hs
@@ -0,0 +1,31 @@
+import Data.List (sortBy)
+
+-- Exercise 1 & 2:
+
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (x:xs) = 1 + myLength xs
+
+-- Exercise 3:
+
+myMean [] = 0
+myMean xs = fromIntegral(sum(xs)) / fromIntegral(length(xs))
+
+-- Exercise 4:
+
+makePalindrome :: [a] -> [a]
+makePalindrome [] = []
+makePalindrome xs = xs ++ reverse xs
+
+-- Exercise 5:
+
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome xs = xs == reverse xs
+
+-- Exercise 6:
+
+orderByLength :: [a] -> [b] -> Ordering
+orderByLength x y = compare (length x) (length y) 
+
+sortByLength :: [[a]] -> [[a]]
+sortByLength xs = sortBy (orderByLength) xs
