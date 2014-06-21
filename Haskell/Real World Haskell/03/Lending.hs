1ca28e3e2fe74e2c31baf605557a8a7831c81e44
Sam Symons <sam@samsymons.com>
2014-06-21 11:57:09 -0700

    Add an example of using guards.

--- Haskell/Real World Haskell/03/Lending.hs
+++ Haskell/Real World Haskell/03/Lending.hs
@@ -3,3 +3,10 @@ lend amount balance = let reserve = 100
                       in if balance < reserve
                          then Nothing
                          else Just newBalance
+
+lendGuard amount balance
+  | amount <= 0 = Nothing
+  | amount > reserve * 0.5 = Nothing
+  | otherwise = Just newBalance
+  where reserve = 100
+        newBalance = balance - amount
