41b0ea19f925f0cfc9dbe366b876b9850f9f83b0
Sam Symons <sam@samsymons.com>
2014-06-27 08:17:09 -0700

    Add some examples of folds.

--- Haskell/Real World Haskell/04/Sum.hs
+++ Haskell/Real World Haskell/04/Sum.hs
@@ -2,3 +2,8 @@ mySum :: (Num a) => [a] -> a
 mySum xs = helper 0 xs
     where helper acc (x:xs) = helper (acc + x) xs
           helper acc _ = acc
+
+foldlSum xs = foldl step 0 xs
+    where step acc x = acc + x
+
+niceSum xs = foldl (+) 0 xs
