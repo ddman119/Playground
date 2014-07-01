3188469428c71497d7d3ee61aaea347ab3ae3e93
Sam Symons <sam@samsymons.com>
2014-07-01 14:35:09 -0700

    Filling out the pretty printing code.

--- Haskell/Real World Haskell/05/Prettify.hs
+++ Haskell/Real World Haskell/05/Prettify.hs
@@ -1,3 +1,41 @@
+data Doc = Empty
+         | Char Char
+         | Text String
+         | Line
+         | Concat Doc Doc
+         | Union Doc Doc
+           deriving (Show, Eq)
+
+empty :: Doc
+empty = Empty
+
+char :: Char -> Doc
+char c = Char c
+
+text :: String -> Doc
+text "" = Empty
+text s = Text s
+
+double :: Double -> Doc
+double d = text (show d)
+
+line :: Doc
+line = Line
+
+string :: String -> Doc
+string str = undefined
+
+(<>) :: Doc -> Doc -> Doc
+Empty <> y = y
+x <> Empty = x
+x <> y = x `Concat` y
+
+hcat :: [Doc] -> Doc
+hcat xs = undefined
+
+fsep :: [Doc] -> Doc
+fsep xs = undefined
+
 punctuate :: Doc -> [Doc] -> [Doc]
 punctuate p [] = []
 punctuate p [d] = [d]
