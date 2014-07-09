e9c2c667252c6cb8d828861f0d568dc40ab12496
Sam Symons <sam@samsymons.com>
2014-07-09 16:23:59 -0700

    Print the output from question 1.

--- Haskell/99 Problems/1.hs
+++ Haskell/99 Problems/1.hs
@@ -1,10 +1,19 @@
 -- Original
 
 myLast :: [a] -> a
+myLast [] = error "No list provided"
 myLast [x] = x
 myLast (_:xs) = myLast xs
 
 -- Improved
 
 myLast' :: [a] -> a
-myLast' xs = head . reverse
+myLast' = head . reverse
+
+-- Output
+
+main :: IO ()
+main = do
+  putStrLn stringValue
+    where lastNumber = myLast [1..10]
+          stringValue = "The last number is: " ++ (show lastNumber)
