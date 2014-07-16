0a57a9d8201bdea203f5d32f2b338b3ba57561d2
Sam Symons <sam@samsymons.com>
2014-07-16 07:09:17 -0700

    Remove some older examples.

--- Haskell/School of Haskell/Advanced Haskell/Functor, Applicative, and Monad/ExampleOne.hs
+++
@@ -1,5 +0,0 @@
-main :: IO ()
-main = do
-  putStrLn "Please enter your birth year: "
-  result <- getLine
-  putStrLn $ "Your age in 2020 will be: " ++ show (2020 - read result)
--- Haskell/School of Haskell/Advanced Haskell/Functor, Applicative, and Monad/ReadMay.hs
+++
@@ -1,8 +0,0 @@
-import Safe (readMay)
-
-main ::IO ()
-main = do
-  print (readMay "1980" :: Maybe Integer)
-  print (readMay "hello" :: Maybe Integer)
-  print (readMay "2000" :: Maybe Integer)
-  print (readMay "two-thousand" :: Maybe Integer)
