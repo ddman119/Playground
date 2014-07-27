5117e3736ee8692f30e85e418dfeab4738e62c09
Sam Symons <sam@samsymons.com>
2014-07-27 11:20:04 -0700

    Try out padding methods on strings.

--- Ruby/Practicing Ruby IO Course/Part 1/Playground/arguments.rb
+++ Ruby/Practicing Ruby IO Course/Part 1/Playground/arguments.rb
@@ -5,5 +5,5 @@ puts
 
 ARGF.each_line do |line|
   puts "File name: #{ARGF.filename}" if ARGF.lineno == 1
-  puts "#{ARGF.lineno}: #{line}"
+  puts "#{ARGF.lineno.to_s.rjust(6)}: #{line}"
 end
---
+++ Ruby/Practicing Ruby IO Course/Part 1/Playground/padding.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+puts " Centering ".center(80, '-')
+puts "Left Align".ljust(80, '-')
+puts "Right Align".rjust(80, '-')
