1766b550b288c703a3d92cf773ac9e049961aea7
Sam Symons <sam@samsymons.com>
2014-08-12 09:52:41 -0700

    Simplifying most of the CSS parsing code.

--- Haskell/Playground/ParseCSS.hs
+++ Haskell/Playground/ParseCSS.hs
@@ -1,29 +1,22 @@
 import Text.ParserCombinators.Parsec
+import Control.Applicative hiding ((<|>))
 
 type Selector = String
 data Rule = Rule String String deriving Show
 data Ruleset = Ruleset Selector [Rule] deriving Show
 
+paddedChar c = char c <* spaces
+
 rule :: Parser Rule
 rule = do
-  key <- many1 letter
-  char ':'
-  spaces
-  
-  value <- many1 (noneOf ";")
-  char ';'
-  spaces
+  key <- many1 letter <* char ':' <* spaces
+  value <- many1 (noneOf ";") <* char ';' <* spaces
 
   return $ Rule key value
 
 ruleset :: Parser Ruleset
 ruleset = do
-  name <- many1 (noneOf "{")
-  char '{'
-  spaces
+  s <- many1 (noneOf "{")
+  r <- paddedChar '{' *> many1 rule <* paddedChar '}'
 
-  r <- many1 rule
-  char '}'
-  spaces
-
-  return $ Ruleset name r
+  return $ Ruleset s r
