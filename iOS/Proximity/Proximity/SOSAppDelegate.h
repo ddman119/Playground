ae799c259a3f5050a80d25780e564195f436de20
Sam Symons <sam@samsymons.com>
2014-08-18 08:11:51 -0700

    Initial Commit

---
+++ iOS/Proximity/Proximity/SOSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SOSAppDelegate.h
+//  Proximity
+//
+//  Created by Sam Symons on 2014-08-18.
+//  Copyright (c) 2014 Sam Symons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SOSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/Proximity/Proximity/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Proximity
+//
+//  Created by Sam Symons on 2014-08-18.
+//  Copyright (c) 2014 Sam Symons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SOSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SOSAppDelegate class]));
+    }
+}
