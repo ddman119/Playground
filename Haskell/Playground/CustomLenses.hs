e1cc5d2432f61780cc8f0aabd38b043468be3252
Sam Symons <sam@samsymons.com>
2014-09-01 10:24:00 -0700

    More Lens experiments.

--- Haskell/Playground/CustomLenses.hs
+++ Haskell/Playground/CustomLenses.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE RankNTypes #-}
+
 data Person = Person { name :: String, age :: Int } deriving (Show)
 data Project = Project { owner :: Person } deriving (Show)
 
-data NaiveLens s a = NaiveLens { view :: s -> a, set :: a -> s -> s }
+data NaiveLens obj prop = NaiveLens { view :: obj -> prop, update :: (prop -> prop) -> obj -> obj }
+
+set' :: NaiveLens obj prop -> prop -> obj -> obj
+set' ln newValue obj = update ln (\_ -> newValue) obj
 
 nameLens :: NaiveLens Person String
-nameLens = NaiveLens name (\a s -> s { name = a })
+nameLens = NaiveLens name (\fn obj -> obj { name = fn (name obj) })
