03c8f3fc9ba976b805506064b938850744e2b364
Sam Symons <sam@samsymons.com>
2014-09-01 22:36:46 -0700

    More Lens shenanigans.

--- Haskell/Playground/CustomLenses.hs
+++ Haskell/Playground/CustomLenses.hs
@@ -3,10 +3,25 @@
 data Person = Person { name :: String, age :: Int } deriving (Show)
 data Project = Project { owner :: Person } deriving (Show)
 
-data NaiveLens obj prop = NaiveLens { view :: obj -> prop, update :: (prop -> prop) -> obj -> obj }
+data Const a b = Const { getConst :: a}
+data Identity a = Identity { runIdentity :: a }
 
-set' :: NaiveLens obj prop -> prop -> obj -> obj
-set' ln newValue obj = update ln (\_ -> newValue) obj
+instance Functor (Const a) where
+  fmap _ (Const a) = Const a
 
-nameLens :: NaiveLens Person String
-nameLens = NaiveLens name (\fn obj -> obj { name = fn (name obj) })
+instance Functor Identity where
+  fmap fn (Identity a) = Identity (fn a)
+
+type Lens obj prop = Functor f => (prop -> f prop) -> obj -> f obj
+
+view :: Lens obj prop -> obj -> prop
+view ln obj = getConst $ ln Const obj
+
+update :: Lens obj prop -> (prop -> prop) -> obj -> obj
+update ln fn obj = runIdentity $ ln (Identity . fn) obj
+
+set :: Lens obj prop -> prop -> obj -> obj
+set ln newValue = update ln (const newValue)
+
+nameLens :: Lens Person String
+nameLens fn person = fmap (\newName -> person { name = newName }) (fn $ name person)
