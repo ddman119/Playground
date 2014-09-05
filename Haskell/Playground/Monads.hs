ab5f069cb37294f2ea644e4b414ca2261fbe46c8
Sam Symons <sam@samsymons.com>
2014-09-04 23:15:29 -0700

    Remove more older unhelpful files.

--- Haskell/Playground/Monads.hs
+++
@@ -1,8 +0,0 @@
-half :: Int -> Maybe Int
-half x = if even x
-            then Just (x `div` 2)
-            else Nothing
-
-main :: IO ()
-main = putStrLn result
-    where result = show (Just 100 >>= half >>= half)
