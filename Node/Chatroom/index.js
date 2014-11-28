99f437cbc97a6a2c6d3b58f6ed71f1aae0ceb195
Sam Symons <sam@samsymons.com>
2014-11-28 09:27:35 -0800

    Finish up the chatroom example.

--- Node/Chatroom/index.js
+++ Node/Chatroom/index.js
@@ -2,9 +2,23 @@ var express = require("express");
 var app = express();
 var port = 4700;
 
+app.set('views', __dirname + '/tpl');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", function(request, response) {
-  response.send("It works!");
+  response.render("page");
 });
 
-app.listen(port);
+var io = require("socket.io").listen(app.listen(port));
 console.log("Listening on port " + port);
+
+// Handle socket.io's traffic:
+
+io.sockets.on('connection', function(socket) {
+  socket.emit('message', { message: "Welcome to the chat" });
+  socket.on('send', function(data) {
+    io.sockets.emit('message', data);
+  });
+});
