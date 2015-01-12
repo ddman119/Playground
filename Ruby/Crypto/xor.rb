32d9342829e37ebbb915d3d6d1660eeeb51d132f
Sam Symons <sam@samsymons.com>
2015-01-11 23:02:00 -0800

    More crypto fun.

--- Ruby/Crypto/xor.rb
+++ Ruby/Crypto/xor.rb
@@ -1,9 +1,13 @@
 first = "1c0111001f010100061a024b53535009181c"
 second = "686974207468652062756c6c277320657965"
 
+first = "61747461636b206174206461776e"
+second = "6c73d5240a948c86981bc294814d"
+
 def xor_hex_strings(first, second)
   zipped = hex_to_binary(first).bytes.zip(hex_to_binary(second).bytes)
-  zipped.map{ |a,b| a ^ b }.pack('C*')
+  result = zipped.map { |a,b| a ^ b }.pack('C*')
+  [result].pack('H*')
 end
 
 def hex_to_binary(hex_string)
