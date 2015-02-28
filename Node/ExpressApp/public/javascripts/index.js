d8acb8c3cff07bb2196f716773b74aa7b95c70d1
Sam Symons <sam@samsymons.com>
2015-02-27 19:53:25 -0800

    Remove Node, that stuff is way out of date now.

--- Node/ExpressApp/public/javascripts/index.js
+++
@@ -1,9 +0,0 @@
-var webrtc = new SimpleWebRTC({
-  localVideoEl: 'localVideo',
-  remoteVideosEl: 'remoteVideos',
-  autoRequestMedia: true
-});
-
-webrtc.on('readyToCall', function () {
-  webrtc.joinRoom('perch');
-});
