96b3d80f1ef1b7e26ba35937a8dbfe7bbd5b09cf
Sam Symons <sam@samsymons.com>
2015-05-16 10:49:10 -0700

    More Rust tomfoolery.

---
+++ Rust/Guess/src/main.rs
@@ -0,0 +1,24 @@
+extern crate rand;
+
+use std::io::prelude::*; 
+use std::io;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    print!("Please enter a guess: ");
+
+    io::stdout().flush()
+        .ok()
+        .expect("Could not flush stdout");
+
+    let random_number = rand::thread_rng().gen_range(1, 101);
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .ok()
+        .expect("Failed to read your guess.");
+
+    print!("You guessed {}", guess);
+    println!("The secret number was {}", random_number);
+}
