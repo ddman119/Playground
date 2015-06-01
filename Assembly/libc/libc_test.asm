886fec97b307ec6310f657a64e6f765eb671517a
Sam Symons <sam@samsymons.com>
2015-06-01 14:00:32 -0700

    More libc fun.

--- Assembly/libc/libc_test.asm
+++ Assembly/libc/libc_test.asm
@@ -1,15 +1,22 @@
 extern _printf
 
 section .data
-  msg: db "Hello, world!", 10
+  msg: db "Hello, world", 10
+  fmt: db "%s", 10, 0
 
 section .text
   global start
 
 start:
+  push rbp
+
   ; Print a string via printf.
-  mov rdi, [rel msg]
+  mov rsi, [rel msg]
+  mov rdi, fmt
+  mov rax, 0
   call _printf
+
+  pop rbp
 
   ; Exit.
   mov rax, 0x2000001
