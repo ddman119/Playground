fdba34b56a9d6f8d94ca8d19ddacc06c38f8bbd0
Sam Symons <sam@samsymons.com>
2015-05-31 22:22:43 -0700

    Try out calls to libc.

--- Assembly/libc/libc_test.asm
+++ Assembly/libc/libc_test.asm
@@ -1,7 +1,17 @@
+extern _printf
+
+section .data
+  msg: db "Hello, world!", 10
+
 section .text
   global start
 
 start:
+  ; Print a string via printf.
+  mov rdi, [rel msg]
+  call _printf
+
+  ; Exit.
   mov rax, 0x2000001
-  mov rdi, 1
+  mov rdi, 0
   syscall
